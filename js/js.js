92c423c046e4c692f72aae9177bc11f6e3177cdb
Mark Heckmann <heckmann.mark@gmail.com>
2014-01-18 12:01:58 +0100

    frist commit

---
+++ js/js.js
@@ -0,0 +1,211 @@
+
+constructs2 =[ 
+[ "sachlicher argumentieren (44)", "(44) polemisch, populitisch" ],
+[ "glaubwürdig und sachlich (60)", "(60) polemisch, unglaubwürdig" ],
+[ "(77) sachlich (klare Argumente)", "populistisch (Unterstellungen, Manipulation, Rhetorik) (77)" ],
+[ "sachlich, deutliches politisches Konzept (100)", "(100) polemisch, politische Idee wird nicht klar" ],
+[ "(75) breite Parteispitze", "personifizierte Politik (75)" ],
+[ "viel Wirtschaftkompetenz (70)", "(70) geringe Wirtschaftskompetenz" ],
+[ "(71) Vorschläge zur Beilegung der Krise vermitteln", "keine wirklichen Lösungsansätze und Vorschläge für aktuelle Krise (71)" ],
+[ "thematische Vielfalt, Fähigkeiten in mehreren Bereichen (58)", "(58) Ein-Punkt-Politik" ],
+[ "(98) breit gefächerte Inhalte/Programm", "Ein-Punkt-Politik (98)" ],
+[ "besitzen Wirtschaftskompetenz (88)", "(88) wenig Wirtschaftskompetenz" ],
+[ "breit gefächerte Kompetenz (66)", "(66) Zentrierte Kompetenz (nur wenige Bereiche)" ],
+[ "besitzen Wirtschaftskompetenz (80)", "(80) geringe Wirtschaftskompetenz" ],
+[ "(39) Aufmerksamkeit richtet sich nicht nur auf eine Person", "Politik ist personifiziert (39)" ],
+[ "(47) Präsenz auf mehrere Köpfe verteilt", "personifizierte Politik (47)" ],
+[ "(7) sichtbare Machtverteilung in der Partei", "personifizierte Politik (7)" ],
+[ "wenig personifizierte Politik, der Basis geöffnet (97)", "(97) personifizierte Politik" ],
+[ "(67) mehrere bekannte Führungspersönlichkeiten", "personifizierte Politik (67)" ],
+[ "(87) gestreute, breite Führungsriege", "personifizierte Ein-Mann Show (87)" ],
+[ "nicht an der Person orientierte (20)", "(20) personifizierte Politik" ],
+[ "(57) auf mehrere Köpfe verteilt, keine eindeutige Hierarchie", "personifizierte Politik (One-Man-Show) (57)" ],
+[ "Fähigkeit zur Umsetzung der eigenen politischen Idee (10)", "(10) Unglaubwürdig, weil keine Fähigkeit Ideen umzusetzen" ],
+[ "(16) setzen das wofür sie stehen um, wenn an der Macht", "Zweifel an Umsetzbarkeit der Ideen (16)" ],
+[ "haben die Fähigkeit eigenen politische Ideen umzusetzen (96)", "(96) scheitern an der Fähigkeit Ideen umzusetzen" ],
+[ "(76) hohe Medienpräsenz", "geringe Medienpräsenz (76)" ],
+[ "(3) man weiß, was sie umsetzen wollen", "kein klares politisches Profil (3)" ],
+[ "(40) nachvollziehbare Wertorientierung", "unklar, nach welchen Werten sie sich richten (40)" ],
+[ "können das wirklich umsetzen, was sie propagieren (36)", "(36) scheitern an ihren eigenen Ideen" ],
+[ "Fähigkeit politische Ideen in Praxis umzusetzen (78)", "(78) Scheitern an der Umsetzung der eigenen Ideen" ],
+[ "schaffen es politische Ideen umzusetzen (86)", "(86) schaffen es nicht, die politischen Ideen umzusetzen" ],
+[ "(38) mehrere Schwerpunktthemen", "Einpunkt-Partei, ein Thema ist Aushängeschild (38)" ],
+[ "Fähigkeit der Umsetzung der eigenen Politik (30)", "(30) man traut ihnen nicht, dass sie ihre Politik umsetzen können" ],
+[ "(25) machen Politik mit mehreren Schwerpunktthemen", "betreiben Ein-Punkt-Politik (25)" ],
+[ "haben Vorstellung davon, wie sie ihre Ideen politisch umsetzen können (48)", "(48) schaffen es nicht, eigene Glaubwürdigkeit rüberzubringen i.S.v. umsetzbarer Idee" ],
+[ "zutrauen in Umsetzungsfähigkeit der eigenen Politik (56)", "(56) kein Vertrauen in die Fähigkeit, die Politik umzusetzen" ],
+[ "schaffen es Ziele realpolitische umzusetzen (68)", "(68) scheitern an Unfähigkeit eigene Politik umzusetzen" ],
+[ "(46) machen ihr Gesellschaftskonzept verständlich", "vermitteln kein klares Gesellschaftskonzept (46)" ],
+[ "(62) konkrete Zielvorgaben", "vermitteln keine klare Vorstellung von ihrem Zielen (62)" ],
+[ "formulieren klare Forderungen, was sie wollen (50)", "(50) keine klare Position beziehen, keine konkreten Vorschläge machen" ],
+[ "haben Profil, man weiß, was sie wollen (90)", "(90) profillos, die politische Idee bleibt unklar" ],
+[ "überzeugende Reformen für Sozialsystem (74)", "(74) keine überzeugenden Reformen des Sozialsystems" ],
+[ "berücksichtigt die Belange aller Bürger (5)", "(5) Klientelpolitik" ],
+[ "lösen ihre Versprechen ein wenn sie an der Macht sind (37)", "(37) hält die eigenen Versprechen nicht" ],
+[ "(72) versuchen größeres Publikum erreichen", "Klientelpartei (72)" ],
+[ "(85) versuchen viele Interessen zu integrieren", "vertreten Interessen einer kleinen Gruppe (85)" ],
+[ "wirken glaubwürdig (26)", "(26) wirken unglaubwürdig" ],
+[ "(23) Alternativen vorschlagen anstatt nur andere schlecht zu machen", "üben keine konstruktive Kritik (23)" ],
+[ "(55) klare, begründete Standpunkte", "es wird nicht klar, was sie eigentlich wollen (55)" ],
+[ "klar, für was für eine Politik sie stehen (13)", "(13) unklar, willkürlich, nicht stringent, nicht folgerichtig" ],
+[ "eher angemessener Ausgleich zwischen Einkommensschichten (17)", "(17) eher ungenügender Ausgleich zwischen Einkomenschichten" ],
+[ "(15) versucht unterschiedliche Interessen zu integrieren", "berücksicht nicht Interesse des Großteils der Bevölkerung (15)" ],
+[ "bürgernah (21)", "(21) unterstützt nicht die Meinungsbildung von der Basis" ],
+[ "bürgernah, direkter Austauch und Versuch dies umzusetzen (34)", "(34) Expertenpolitik, von oben herab, geschlossener Zirkel" ],
+[ "vertrauenswürdige Führung (33)", "(33) nicht vertrauenswürdige Führung" ],
+[ "(95) vetrauenswürdige Führungspersönlichkeiten", "wenig vertrauenswürdige Führungspersönlichkeit (95)" ],
+[ "widmen sich angemessen der sozialen Frage (Umverteilung) (24)", "(24) fördern gesellschaftliche Spannungen" ],
+[ "persönliche Sympathie für die Person als Mensch (81)", "(81) unsympathisch, Art des Auftretens fremd" ],
+[ "besitzen Umweltkompetenz (43)", "(43) keine Umweltkompetenz" ],
+[ "besitzen Umweltkopemtenz (93)", "(93) geringe Umweltkompetenz" ],
+[ "Parteispitze ist sympathisch (27)", "(27) Unsympathische Parteispitze" ],
+[ "bürgernah (63)", "(63) geschlossene Gesellschaft, abgetrennt" ],
+[ "bürgernah, versucht Programm von der Basis aus zu erstellen (41)", "(41) Programm wird parteiintern von oben herab erstellt" ],
+[ "eröffnen Zukunftsperspektiven (6)", "(6) kein Konzept für zunkunftsfähige Gesellschaft" ],
+[ "haben viel Umweltkompetenz (53)", "(53) wenig Umweltkompetenz" ],
+[ "haben Umweltkompetenz (84)", "(84) Legen wenig Wert auf umweltpolitische Themen" ],
+[ "(1) versucht Meinungen der Bürger rauszufinden und umzusetzen", "keine Bürgernähe (1)" ],
+[ "(51) sympathische Führung", "unsympathische Führung (auf die Person bezogen) (51)" ],
+[ "(64) überzeugende Konzepte", "keine überzeugenden Konzepte (64)" ],
+[ "(94) nachhaltige Ideen", "kein ernstzunehmendes Gesellschaftskonzept für die nächsten Jahrzehnte (94)" ],
+[ "(14) Vorschläge für umfassende Reformen", "kaum umfassende Reformvorschläge (14)" ],
+[ "(31) innovative Reformvorschläge", "haben keine umfassenden Reformideen (31)" ],
+[ "(54) haben revolutionäre Gesellschaftsvorstellungen", "haben keine innovativen neuen Konzepte (54)" ],
+[ "(65) unbestechlich, reinsten Gewissens", "sind bestechlich (65)" ],
+[ "(91) Anreize und Entscheidungen aus der breiten Masse", "gibt Meinung vor, lässt kleinen Kreis entscheiden statt breite Masse (91)" ],
+[ "innovative, alternative politische Konzepte (83)", "(83) Verwaltung des Status Quo" ],
+[ "(61) Alternative Politik, nicht neo-liberaler Mainstream", "haben keine Alternativen zu neo-liberaler Politik (61)" ],
+[ "(99) unabhängig von Lobbyisten", "beinflussbar durch Lobbyisten (99)" ],
+[ "Ideen für umfassendere Reformen (4)", "(4) am Status-Quo rumschrauben, ohne merkbare Veränderungen" ],
+[ "(52) Politik für Geringverdiener", "Politik für Besserverdiener (52)" ],
+[ "(92) gleiche Startvorausetzungen für alle schaffen", "Unterschiedliche Voraussetzungen akzeptieren, um andere nicht auszubremsen (92)" ],
+[ "(2) egalitäre Verteilung von Mitteln und Möglichkeiten", "soziale Stellung durch Konkurrenz (2)" ],
+[ "(32) orientiert Politik an geringen Einkommen", "orientiert sich an der Oberschicht (32)" ],
+[ "(12) Zielgruppe Unterschicht", "Zielgruppe Oberschicht (12)" ],
+[ "orientiert sich an der Unterschicht (22)", "(22) orientiert sich an der Oberschicht" ],
+[ "(42) setzt mehr auf politische Gleichheit (gleiche Möglichkeiten der Einflussnahme)", "setzt mehr auf politische Freiheit (Möglichkeiten der Einflussnahme auf Entscheidungsprozesse) (42)" ],
+[ "(45) unbestehlich, ein Programm unabhängig vom eigenen Nutzen aufstellen", "ist bestechlich, korrumpierbar, verschaftt denen Vorteile, die auch ihr Vorteile verschaffen (45)" ],
+[ "(82) legt mehr Wert auf Gleichheit", "legt Wert auf indiviudelle Freiheit (82)" ],
+[ "kümmert sich um Arbeitnehmerschaft (11)", "(11) übergeht tendenziell Interessen der Arbeitnehmerschaft" ],
+[ "versuch bürgernah und basidemokratisch zu sein (73)", "(73) elitär und abgeschottet" ],
+[ "beziehen Umweltsaspekte mit ein (8)", "(8) wenig Maßnahmen zum Schutz der Umwelt" ],
+[ "legen viel Wert auf Umweltschutz (18)", "(18) vernächlässigen Umweltschutz" ],
+[ "(35) sorgt für sozialen Ausgleich", "sorgt nicht genug für den sozialen Ausgleich (35)" ],
+[ "legen Wert auf ökologische Aspekte (28)", "(28) berücksichtigen kaum ökologische Aspekte" ],
+[ "(9) Offen für neue Rollenbilder", "tradierte Geschlechterrollenbilder erhalten (9)" ],
+[ "(49) setzen auf Selbstbestimmung", "setzen auf Kontrolle der Bürger (49)" ],
+[ "offen für alternative Lebenskonzepte (69)", "(69) konservative Rollenverteilung" ],
+[ "(29) offen für alternative Lebensformen", "haben starres konservatives Gesellschaftskonzept (29)" ],
+[ "offen für nicht traditionelle Familienformen (19)", "(19) Politik ausgerichtet auf traditionelle Familienform" ],
+[ "haben sich verschiedenen Lebensmodellen geöffnet (89)", "(89) betrieben Politik für bestimmten Lebensentwurf" ],
+[ "offen für alternative Lebensformen (59)", "(59) ausgerichtet an einem bestimmten Lebensmodell" ],
+[ "offen für verschiedene Lebensentwürfe (79)", "(79) starres oder enges Konzept von Lebensentwürfen" ] 
+];
+
+
+
+// ---------------------------- //
+
+
+function create_new_construct_box(pole1, pole2, id) {
+	// test if poles are undefined
+	//alert(pole1 + "--" + pole2);
+	//if (pole1 !== undefined & pole2 !== undefined) {	
+	var con = document.createElement( "div" );
+	con.id = "construct_" + id;
+	var poleEmergent = document.createElement( "div" );
+    var poleOpposite = document.createElement( "div" );
+    var dummy = document.createElement( "div" );
+
+    con.className = "construct_div_2 hyphenate";
+    poleEmergent.className = "construct_pole_emergent hyphenate";
+    poleEmergent.lang = "de";
+    poleOpposite.className = "construct_pole_opposite";
+    poleOpposite.title = pole2;
+    dummy.className = "clear_float";
+    poleEmergent.innerHTML = pole1;
+    poleOpposite.innerHTML = "?";
+	con.appendChild(poleEmergent);
+	con.appendChild(poleOpposite);
+	con.appendChild(poleOpposite);
+	return con;	
+}
+
+
+
+/*constructs.length
+var conContainer = document.createElement( "div" );
+conContainer.className = "ungrouped_container";
+
+for (var i = 0; i < useConstructs-1; i++) {
+	var tmp = make_construct_2(constructs[i], i);
+	conContainer.appendChild(tmp);
+}
+document.body.appendChild(conContainer); */
+
+
+
+
+/*
+$(document).ready(function(){
+	$(".construct_div_2").draggable( {
+		stop: function() { 
+			var dragged = $(this);
+			var posDragged = $(this).position();  // position of dragged element
+			var minDist = 10000;
+			var e = $(".construct_div_2");
+			var sameIndex = -1;
+			e.each(function(i, e) {
+				var pos = $(this).position();
+				var dist = Math.sqrt(Math.pow(posDragged.left - pos.left, 2) + Math.pow(posDragged.top - pos.top, 2));
+				var sameId = $(this).attr("id") == dragged.attr( "id");
+				if (sameId)
+					sameIndex = i;
+				if (dist < minDist && !sameId){
+					minDist = dist;
+					if (minDist < 60)
+						dragged.css( "background-color", $(this).css( "background-color")); 
+				}
+				var color;
+				if (minDist < 60) {	
+					 // change color if near
+					//dragged.css( "background-color", $(this).css( "background-color")); 
+				} else {
+					if (sameIndex > 0) {
+						dragged.css( "background-color", colors[sameIndex]);																
+					}
+				}
+			});
+			
+			$("#positions_div_sameIndex").text("index: " + sameIndex);
+			$("#positions_div_id").text("ID: " + dragged.attr( "id"));
+			$("#positions_div_left").text("Left: " + posDragged.left);
+			$("#positions_div_top").text("  |  Top: " + posDragged.top);
+			$("#positions_div_all").text("  | All: " + get_pos());
+			$("#positions_div_mindist").text("  | Min dist: " + Math.round(minDist));
+			
+
+			// check if any other div is near, if yes adopt the color, if no assign new color
+			
+		}
+	});       // make all constructs draggable
+	
+	$(".construct_pole_opposite").tooltip({
+			placement: 'right',
+			container: "#construct_5"
+	});
+	delete_old_constructs("ungrouped_container");
+	//$(".ungrouped_container").height($(window).height());
+});
+*/
+
+function convertHex(hex,opacity){
+    hex = hex.replace('#','');
+    r = parseInt(hex.substring(0,2), 16);
+    g = parseInt(hex.substring(2,4), 16);
+    b = parseInt(hex.substring(4,6), 16);
+    result = 'rgba('+r+','+g+','+b+','+opacity/100+')';
+    return result;
+}
+
+
